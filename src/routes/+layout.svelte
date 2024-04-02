97e0d6f07531870fc328ffe08dd1471dc63798a5
JonaVdv <j.vdv@outlook.com>
2024-04-02 12:08:08 +0200

    Add layout route

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { LayoutData } from './$types.d.ts';
+    import '../styles/main.css'
+    
+    export let data: LayoutData;
+</script>
+<main class="main">
+    <slot/>
+</main>
