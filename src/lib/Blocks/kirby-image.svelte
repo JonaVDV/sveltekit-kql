4d86e63ab4b417479925d7164928f0539267ca44
JonaVdv <j.vdv@outlook.com>
2024-04-02 16:26:20 +0200

    Add kirby-image component

---
+++ src/lib/Blocks/kirby-image.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { KirbyBlock } from "kirby-types";
+	import {page} from '$app/stores';
+	import ImageResolver from "./image-resolver.svelte";
+	interface $$Props extends KirbyBlock<'image'> {
+		id: string;
+		alt: string;
+		src: string;
+		location: string;
+		caption: string;
+		ratio: string;
+		image: string[];
+	}
+	export let id: $$Props['id'];
+	export let alt: $$Props['content']['alt'];
+	export let src: $$Props['content']['src'];
+	export let location: $$Props['content']['location'];
+	export let caption: $$Props['content']['caption'];
+	export let ratio: $$Props['content']['ratio'];
+	export let image: $$Props['content']['image'];
+	
+	let images = $page.data['page("/about")'].result.images;
+	
+	let width: number
+</script>
+
+<figure bind:clientWidth={width}>
+	<ImageResolver let:image collection={images} uuid={image[0]}>
+		<img src="{image?.url}" alt="">
+	</ImageResolver>
+</figure>
+
+
