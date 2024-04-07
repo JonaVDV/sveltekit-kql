1a6b481e4ad47443263bca8dfa10c0793fade28c
JonaVdv <j.vdv@outlook.com>
2024-04-07 16:46:38 +0200

    Refactor kirby-image.svelte component to include link attribute

--- src/lib/Blocks/kirby-image.svelte
+++ src/lib/Blocks/kirby-image.svelte
@@ -1,33 +1,52 @@
 <script lang="ts">
-	import type { KirbyBlock } from "kirby-types";
-	import {page} from '$app/stores';
-	import ImageResolver from "./image-resolver.svelte";
+	import type { KirbyBlock } from '$lib/types';
+	import { page } from '$app/stores';
+	import ImageResolver from './image-resolver.svelte';
 	interface $$Props extends KirbyBlock<'image'> {
 		id: string;
 		alt: string;
 		src: string;
-		location: string;
 		caption: string;
 		ratio: string;
 		image: string[];
+		link: string
+		location: string;
 	}
 	export let id: $$Props['id'];
-	export let alt: $$Props['content']['alt'];
-	export let src: $$Props['content']['src'];
-	export let location: $$Props['content']['location'];
-	export let caption: $$Props['content']['caption'];
-	export let ratio: $$Props['content']['ratio'];
-	export let image: $$Props['content']['image'];
-	
+	export let alt: $$Props['alt'];
+	export let caption: $$Props['caption'];
+	export let ratio: $$Props['ratio'];
+	export let image: $$Props['image'];
+	export let link: $$Props['link'];
+	export const location: $$Props['location'] = '';
+	export const src: $$Props['src'] = '';
+
 	let images = $page.data['page("/about")'].result.images;
-	
-	let width: number
+
+	if (!alt) {
+		console.warn('The alt attribute is missing on the image');
+	}
+
+	if (ratio && ratio.match(/^\d+\/\d+$/) === null) {
+		throw new TypeError('The ratio attribute must be in the format "width/height"');
+	}
 </script>
 
-<figure bind:clientWidth={width}>
-	<ImageResolver let:image collection={images} uuid={image[0]}>
-		<img src="{image?.url}" alt="">
-	</ImageResolver>
+<figure>
+	{#if link}
+		<a href="{link}">
+			<ImageResolver let:image collection={images} uuid={image[0]}>
+				<img src={image?.url} {alt} {id} />
+			</ImageResolver>
+		</a>
+	{:else}
+		<ImageResolver let:image collection={images} uuid={image[0]}>
+			<img src={image?.url} {alt} {id} />
+		</ImageResolver>
+	{/if}
+	{#if caption}
+		<figcaption>
+			{caption}
+		</figcaption>
+	{/if}
 </figure>
-
-
