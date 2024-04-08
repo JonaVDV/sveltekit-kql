3cd4ca65fbf0894ac12c2b22c5349988d7626610
JonaVdv <j.vdv@outlook.com>
2024-04-08 17:34:57 +0200

    Add docs to components store and add better types

--- src/lib/stores/components.ts
+++ src/lib/stores/components.ts
@@ -1,4 +1,16 @@
-import type { ComponentType } from "svelte";
-import { writable } from "svelte/store";
+import type { KirbyDefaultBlocks } from 'kirby-types';
+import type { ComponentType } from 'svelte';
+import { writable } from 'svelte/store';
 
-export const components = writable<Record<string, ComponentType>>({});
+/**
+ * Stores all the components that will be used for rendering content from kirby cms
+ * @example
+ * import { components } from '$lib/stores/components';
+ *
+ * $components = {
+ *   ...$components,
+ *   text: TextBlock,
+ *   image: ImageBlock,
+ * }
+ */
+export const components = writable<Partial<Record<keyof KirbyDefaultBlocks, ComponentType>>>();
