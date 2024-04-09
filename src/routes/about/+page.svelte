65dcae4e41860047adcae6678be48a66605b6970
JonaVdv <j.vdv@outlook.com>
2024-04-09 16:56:58 +0200

    clean up about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
-    import type { KirbyQuerySchema } from 'kirby-types';
     import type {KQLData} from './$kql'
 	import {KirbyLayouts} from '$lib';
     export let data: KQLData
-    // omit all the keys of pagedata that are of the type KirbyQuerySchema
 
-    $: console.log(data);
-    
     $: layouts = data['page("/about")'].result.layouts
     
     
 </script>
 
-<KirbyLayouts {layouts} class="test"/>
-
-<style>
-    .test{
-        background-color: red;
-    }
-</style>
+<KirbyLayouts --grid-gap="3rem" {layouts}/>
