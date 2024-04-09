570e5f9fcc87aeb5ab23e9121a69d9d52a7fac92
JonaVdv <j.vdv@outlook.com>
2024-04-09 17:33:44 +0200

    add contact section to complete about page of kirby starter kit

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,47 @@
 <script lang="ts">
-    import type {KQLData} from './$kql'
-	import {KirbyLayouts} from '$lib';
-    export let data: KQLData
+	import type { KQLData } from './$kql';
+	import { KirbyLayouts } from '$lib';
+	export let data: KQLData;
 
-    $: layouts = data['page("/about")'].result.layouts
-    
-    
+	$: layouts = data['page("/about")'].result.layouts;
+
+	$: aboutPage = data['page("/about")'].result;
 </script>
 
-<KirbyLayouts --grid-gap="3rem" {layouts}/>
+<KirbyLayouts --grid-gap="3rem" {layouts} />
+
+<aside class="contact">
+	<h2 class="h1">Get in contact</h2>
+	<div class="grid" style="--gutter: 1.5rem">
+		<section class="column text" style="--columns: 4">
+			<h3>Address</h3>
+			<div>
+        {@html aboutPage.address}
+      </div>
+		</section>
+
+		<section class="column text" style="--columns: 4">
+			<h3>Email</h3>
+      <p>
+        {@html aboutPage.email}
+      </p>
+			<h3>Phone</h3>
+			<p>
+				{@html aboutPage.phone}
+			</p>
+		</section>
+
+		<section class="column text" style="--columns: 4">
+			<h3>On the web</h3>
+			<ul>
+        {#each aboutPage.social as item, index}
+          <li>
+            <a href={item.url}>
+              {item.platform}
+            </a>
+          </li>
+        {/each}
+			</ul>
+		</section>
+	</div>
+</aside>
