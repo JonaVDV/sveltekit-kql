9fb957369eae63fc304ffad7abb29aff550a8aec
JonaVdv <j.vdv@outlook.com>
2024-04-09 18:56:47 +0200

    Refactor +layout.svelte to import KQLLayoutData type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import type { LayoutData } from './$types.d.ts';
+    import type { KQLLayoutData } from './$kql.d.ts'
     import '../styles/main.css'
 	import { components } from '$lib';
     import KirbyImage from '$lib/Blocks/kirby-image.svelte';
@@ -7,16 +7,24 @@
     import KirbyQuote from '$lib/Blocks/kirby-quote.svelte';
     import KirbyHeading from '$lib/Blocks/kirby-heading.svelte';
 
-    
-    export let data: LayoutData;
+    export let data: KQLLayoutData;
 
     $components = {
         image: KirbyImage,
         text: KirbyText,
         quote: KirbyQuote,
-        heading: KirbyHeading
+        heading: KirbyHeading,        
     }
+    
 </script>
+
+<svelte:head>
+    <title>
+        {data.site.result.title}
+    </title>
+</svelte:head>
+
+
 <main class="main">
     <slot/>
 </main>
