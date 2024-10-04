0f84fa4a679d0bc35b4dd3006077dafbd22464d5
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-10-04 12:37:12 +0200

    feat: add some files upfront for types

---
+++ src/lib/types/allowedMethods.ts
@@ -0,0 +1,69 @@
+interface allowedMethodsForChildren {
+	children: any;
+	childrenAndDrafts: any;
+	draft: any;
+	drafts: any;
+	find: any;
+	findPageOrDraft: any;
+	grandChildren: any;
+	hasChildren: any;
+	hasDrafts: any;
+	hasListedChildren: any;
+	hasUnlistedChildren: any;
+	index: any;
+	search: any;
+}
+
+interface allowedMethodsForFiles {
+	audio: any;
+	code: any;
+	documents: any;
+	file: any;
+	files: any;
+	hasAudio: any;
+	hasCode: any;
+	hasDocuments: any;
+	hasFiles: any;
+	hasImages: any;
+	hasVideos: any;
+	image: any;
+	images: any;
+	videos: any;
+}
+
+interface allowedMethodsForModels {
+	apiUrl: any;
+	blueprint: any;
+	content: any;
+	dragText: any;
+	exists: any;
+	id: any;
+	mediaUrl: any;
+	modified: any;
+	permissions: any;
+	panel: any;
+	permalink: any;
+	previewUrl: any;
+	url: any;
+}
+
+interface AllowedMethodsForSiblings {
+	indexOf: any;
+	next: any;
+	nextAll: any;
+	prev: any;
+	prevAll: any;
+	siblings: any;
+	hasNext: any;
+	hasPrev: any;
+	isFirst: any;
+	isLast: any;
+	isNth: any;
+}
+
+interface AllowedMethodsForParents {
+	parent: any;
+	parentId: any;
+	parentModel: any;
+	site: any;
+}
