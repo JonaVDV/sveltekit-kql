cb7a08416f7e57ddb6065edcf222cc003b212b29
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-10-12 23:30:32 +0200

    feat: export types in query-resolver for improved accessibility

--- src/lib/types/query-resolver.ts
+++ src/lib/types/query-resolver.ts
@@ -2,13 +2,13 @@ import type { Collection } from './collection';
 /**
  * Get the key from the query or if it is not there, return the type of the __extra property
  */
-type GetKeyFromQueryOrExtra<T extends { __extra: any }, K> = K extends keyof T
+export type GetKeyFromQueryOrExtra<T extends { __extra: any }, K> = K extends keyof T
 	? T[K]
 	: T['__extra'];
 /**
  * used to check if a value of a key is a boolean if it is, we return true, otherwise false
  */
-type IsKeyBooleanType<TObject extends Record<string, any>, Key> = Key extends keyof TObject
+export type IsKeyBooleanType<TObject extends Record<string, any>, Key> = Key extends keyof TObject
 	? TObject[Key] extends boolean
 		? true
 		: false
@@ -18,7 +18,7 @@ type IsKeyBooleanType<TObject extends Record<string, any>, Key> = Key extends ke
  *
  * Get the __default property from a query object if it exists, otherwise get the __extra property
  */
-type GetQueryTypeDefaultOrExtra<TQuery> = TQuery extends { __default: infer Default }
+export type GetQueryTypeDefaultOrExtra<TQuery> = TQuery extends { __default: infer Default }
 	? Default
 	: TQuery extends { __extra: infer Extra }
 		? Extra
@@ -27,9 +27,9 @@ type GetQueryTypeDefaultOrExtra<TQuery> = TQuery extends { __default: infer Defa
 /**
  * gets the __default property from an object if it exists, otherwise it returns the object itself
  */
-type ExtractDefault<T> = T extends { __default: infer D } ? D : T;
+export type ExtractDefault<T> = T extends { __default: infer D } ? D : T;
 
-type FunctionToReturn<T> = T extends (...args: any) => any ? ReturnType<T> : T;
+export type FunctionToReturn<T> = T extends (...args: any) => any ? ReturnType<T> : T;
 
 /**
  * CompareAndGetFromQuery takes a select object, a key, and a query object,
@@ -61,12 +61,13 @@ export type KQLQueryTypeResolver<T extends KQLQuery> = WrapIfCollection<
 	T['query']
 >;
 
-type IsCollection<T> = T extends Collection<any> ? true : false;
-type ToCollection<T> = T extends Collection<infer U> ? U : T;
+export type IsCollection<T> = T extends Collection<any> ? true : false;
+export type ToCollection<T> = T extends Collection<infer U> ? U : T;
 /**
  * WrapIfCollection takes a type and a query and returns the type as an array if the query is a collection
  */
-type WrapIfCollection<TObject, Query> = IsCollection<Query> extends true ? TObject[] : TObject;
+export type WrapIfCollection<TObject, Query> =
+	IsCollection<Query> extends true ? TObject[] : TObject;
 
 /**@todo - make sure this is usable in `createMultiQueryLoad` */
 type MultiQueryTypeResolver<T extends Record<string, KQLQuery> & { options: any }> = {
