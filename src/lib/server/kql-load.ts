a145f284583919bb58ffb823661de29b7fb9c56a
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-10-12 20:34:34 +0200

    feat: enhance the types of kql load for the ultimate typesaftey

--- src/lib/server/kql-load.ts
+++ src/lib/server/kql-load.ts
@@ -2,6 +2,7 @@ import type { ServerLoad, ServerLoadEvent } from '@sveltejs/kit';
 import { kqlHandler, type KQLClientOptions } from './kql-handler';
 import type { KirbyQueryRequest } from 'kirby-types';
 import { transformQuery } from './utils';
+import type { KQLQuery, KQLQueryTypeResolver } from '../types/query-resolver';
 
 export interface KQLLoadOptions extends KQLClientOptions {
 	/**
@@ -10,7 +11,7 @@ export interface KQLLoadOptions extends KQLClientOptions {
 	 * @returns The transformed data
 	 */
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- We don't know the shape of the data since it's json
-	transform?: (data: any, event: ServerLoadEvent) => any;
+	transform?: <T>(data: T, event: ServerLoadEvent) => T;
 }
 
 /**
@@ -19,10 +20,11 @@ export interface KQLLoadOptions extends KQLClientOptions {
  * @param options - Options for the KQL client
  * @returns A server load function that fetches the KQL data and additional data when specified
  */
-export function kqlLoad(query: KirbyQueryRequest, options: KQLLoadOptions = {}) {
+export function kqlLoad<T extends KQLQuery>(query: T, options: KQLLoadOptions = {}) {
 	return async (event: ServerLoadEvent) => {
 		const { transform, ...clientOptions } = options;
 		const { fetch, depends } = event;
+		type Content = KQLQueryTypeResolver<T>;
 		const transformedQuery = transformQuery(query);
 		// Create a unique dependency key for this query
 		const dependencyKey = `kql:${JSON.stringify(transformedQuery.query)}` as const;
@@ -31,7 +33,7 @@ export function kqlLoad(query: KirbyQueryRequest, options: KQLLoadOptions = {}) 
 		const data = await kqlHandler(transformedQuery, { ...clientOptions, fetch });
 
 		return {
-			kqlData: transform ? transform(data, event) : data.result,
+			kqlData: data.result as Content,
 			code: data.code,
 			status: data.status
 		};
