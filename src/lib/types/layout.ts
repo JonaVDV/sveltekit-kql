73c10595f5ce9cdc8735e29ced591fd9d4da6075
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-10-17 22:57:21 +0200

    feat: add types for layouts and layout columns

--- src/lib/types/layout.ts
+++ src/lib/types/layout.ts
@@ -1,22 +1,33 @@
 import type { AllowedMethodsForSiblings } from './allowedMethods';
+import type { Blocks } from './block';
 import type { Collection } from './collection';
 import type { Content } from './content';
+import type { Field } from './field';
 
 export interface Layout extends AllowedMethodsForSiblings {
 	__default: {
 		attrs: () => Content;
 		columns: () => LayoutColumns;
 		id: () => string;
-		isEmpty: () => boolean;
-		isNotEmpty: () => boolean;
-		parent: () => Content;
 	};
+	__extra: Field;
+	attrs: () => Content;
+	columns: () => LayoutColumns;
+	id: () => string;
+	isEmpty: () => boolean;
+	isNotEmpty: () => boolean;
+	parent: () => Content;
 }
 
 export interface Layouts extends Collection<Layout> {}
 
 interface LayoutColumn extends AllowedMethodsForSiblings {
-	blocks: () => any;
+	__default: {
+		blocks: () => Blocks;
+		id: () => string;
+		width: () => string;
+	};
+	blocks: () => Blocks;
 	id: () => string;
 	isEmpty: () => boolean;
 	isNotEmpty: () => boolean;
