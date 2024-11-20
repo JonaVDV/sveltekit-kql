7bb974569163621d681e459cb50ab5deeece0b71
Jona van der Vlist - 1075709 <j.vdv@outlook.comgit>
2024-11-20 22:12:01 +0100

    WIP: (demo) about page

--- src/routes/demo/about/+page.svelte
+++ src/routes/demo/about/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { KirbyLayouts } from '$lib/components';
 	import type { PageData } from './$types';
+	import { KirbyBlocks } from '$lib/components';
 	interface Props {
 		data: PageData;
 	}
@@ -10,3 +12,42 @@
 
 	let aboutPage = $derived(data.page);
 </script>
+
+<KirbyLayouts {layouts}>
+	{#snippet layoutWrapper(snippet, layout)}
+		{@const columns = layout.columns.length}
+		<div class="grid" style="--max-columns: {columns}">
+			{@render snippet(layout)}
+		</div>
+	{/snippet}
+
+	{#snippet columnWrapper(snippet, column)}
+		<div class="column">
+			{@render snippet(column)}
+		</div>
+	{/snippet}
+
+	{#snippet children(column)}
+		{@const blocks = column.blocks}
+
+		<KirbyBlocks {blocks} />
+	{/snippet}
+</KirbyLayouts>
+
+<style>
+	.grid {
+		--max-columns: 1;
+		display: grid;
+		gap: 3rem;
+	}
+
+	@media (min-width: 768px) {
+		.grid {
+			grid-template-columns: repeat(var(--max-columns), 1fr);
+		}
+	}
+
+	.grid > .column {
+		margin-block-end: 3rem;
+	}
+</style>
